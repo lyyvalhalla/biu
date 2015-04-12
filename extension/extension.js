dc970a321d00e2084d1cd154088eeb9545f2692d
Ruoyu Li <ruoyu.li@snapchat.com>
2015-04-12 05:17:31 -0400

    working on exporting

--- extension/extension.js
+++ extension/extension.js
@@ -1,3 +1,5 @@
+pattern = '127.0.0.1';
+
 // send json to middleman
 chrome.pageAction.onClicked.addListener(function(tab) {
     chrome.bookmarks.getTree(function(bookmarkTreeNodes) {
@@ -13,10 +15,30 @@ chrome.pageAction.onClicked.addListener(function(tab) {
 
 // accept json from website, then over-write user bookmarks
 chrome.runtime.onMessageExternal.addListener(function(request, sender, sendResponse) {
-    console.log(request);
+    bookmarkTreeNodes = JSON.parse(request.bookmarks);
+    for (var i=0; i<bookmarkTreeNodes[0].children.length; i++) {
+        default_node = bookmarkTreeNodes[0].children[i];
+        for (var j=0; j<default_node.children.length; j++) {
+            modifiable_node = default_node.children[j];
+            console.log(modifiable_node);
+        }
+    }
 });
 
-// NOTE: ignore this function
+function createBookmarkNode(node) {
+    // base case: not a folder or an empty folder, create it and stop
+    if (node.children === undefined || node.children.length === 0) {
+        chrome.bookmarks.create(
+            {'parentId': node.parentId,
+             'title': 'Extension bookmarks'},
+             function(newFolder) {
+                console.log("added folder: " + newFolder.title);
+            });
+    }
+
+}
+
+// NOTE: ignore this function, do not touch
 // When the extension is installed or upgraded ...
 chrome.runtime.onInstalled.addListener(function() {
   // Replace all rules ...
@@ -27,7 +49,7 @@ chrome.runtime.onInstalled.addListener(function() {
         // That fires when a page's URL contains a 'g' ...
         conditions: [
           new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { hostEquals: '127.0.0.1' },
+            pageUrl: { hostEquals: pattern },
           })
         ],
         // And shows the extension's page action.
--- website/script.js
+++ website/script.js
@@ -18,15 +18,92 @@ window.onload = function() {
     });
 };
 
-$(document).ready(function() {
-    $("button").click(function() {
-        sendBookmarkToExtension({});
-    });
-});
-
+// send the bookmark json back to the extension
 function sendBookmarkToExtension(jsonObject) {
     jsonString = JSON.stringify(jsonObject);
     chrome.runtime.sendMessage(extension_id, {bookmarks: jsonString}, function(response) {
         // console.log(response);
     });   
 }
+
+// NOTE:
+// this function is used to demonstrate the effect of sending modified bookmark
+// json back to the extension, after it's successfully sent, the bookmarks
+// should be over-written
+$(document).ready(function() {
+    var testBookmarkJson = [
+    {
+        "children": [
+        {
+            "children": [
+            {
+                "dateAdded": 1426460293083,
+                "id": "7",
+                "index": 0,
+                "parentId": "1",
+                "title": "＼(~o~)／ Code Lab",
+                "url": "file:///Users/ruoyu.li/Dropbox/lyy_by_lry/index.html"
+            },
+            {
+                "dateAdded": 1428649468630,
+                "id": "9",
+                "index": 1,
+                "parentId": "1",
+                "title": "google",
+                "url": "https://www.google.com/"
+            },
+            {
+                "children": [],
+                "dateAdded": 1428792259995,
+                "dateGroupModified": 1428792259995,
+                "id": "11",
+                "index": 2,
+                "parentId": "1",
+                "title": "test"
+            }
+            ],
+            "dateAdded": 1416532596847,
+            "dateGroupModified": 1428714291760,
+            "id": "1",
+            "index": 0,
+            "parentId": "0",
+            "title": "Bookmarks Bar"
+        },
+        {
+            "children": [],
+            "dateAdded": 1416532596847,
+            "id": "2",
+            "index": 1,
+            "parentId": "0",
+            "title": "Other Bookmarks"
+        },
+        {
+            "children": [
+            {
+                "dateAdded": 1411875397138,
+                "id": "5",
+                "index": 0,
+                "parentId": "3",
+                "title": "Hacker News Mobile - iHackerNews",
+                "url": "http://ihackernews.com/"
+            }
+            ],
+            "dateAdded": 1416532596847,
+            "dateGroupModified": 1418357628958,
+            "id": "3",
+            "index": 2,
+            "parentId": "0",
+            "title": "Mobile Bookmarks"
+        }
+        ],
+        "dateAdded": 1428818872229,
+        "id": "0",
+        "title": ""
+    }
+    ];
+
+    $("#sendButton").click(function() {
+        sendBookmarkToExtension(testBookmarkJson);
+    });
+});
+
